cc5b1b9d7fd6f4afaeadde1e9d7935b8160dcfad
Michael Botha <mbotha@sataxi.co.za>
2021-06-06 21:39:59 +0200

    update

--- Investmentcalculator.py
+++ Investmentcalculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[25]:
+# In[26]:
 
 
 #Single selections dropdown working with freq
@@ -318,97 +318,4 @@ def display_page(pathname):
 
 if __name__ == "__main__":
     app.run_server(debug=False)
-
-
-# In[ ]:
-
-
-
-
-@app.callback([Output('Age-output-container', 'children'),
-               Output('RetAge-output-container', 'children'),
-               Output('LifeExpectancy-output-container', 'children'),
-               Output('AmountsliderType-output-container', 'children'),
-               Output('GrowthRate-output-container', 'children'),
-               Output('Inflation-output-container', 'children'),
-               Output('Projected-retirement-assets', 'figure'),
-               Output('Retirement-Income-assets', 'figure')],
-              [Input('Age-slider', 'value'),
-               Input('RetAge-slider', 'value'),
-               Input('LifeExpectancy-investment-slider', 'value'),
-               Input('Investment-type-dropdown', 'value'),
-               Input('Principal-investment-slider', 'value'),
-               Input('GrowthRate-slider', 'value'),
-               Input('Inflation-slider', 'value'),
-               Input('TargetretirementIncome-slider', 'value')])
-
-def CalculateRetirement(Client_age,Client_Retirement_Age, InvestmentType ,InvestmentAmount,TermInRetirement, Client_GrowthRate,Client_Inflation , Client_target_Income ):
-    
-    age = Client_age
-    RetireMentAge = Client_Retirement_Age
-    MaxDrawdown = 0.175
-    MinDrawdown = 0.025
-    Fundvalue = InvestmentAmount
-    period = RetireMentAge - age + 1 + TermInRetirement
-    GrowthRate = Client_GrowthRate
-    Inflation = Client_Inflation
-    RealReturn = ((1+ GrowthRate)/(1+Inflation ) -1)
-    N_rows = period
-    N_cols = 6
-    ValuesMatrix = pd.DataFrame(np.zeros((N_rows, N_cols)))
-    TargetIncome = Client_target_Income
-
-    for x in range(period):
-    
-        if (x + age) <  RetireMentAge :
-            ValuesMatrix.iloc[x][0] = age + x
-    
-            if x == 0 :
-                ValuesMatrix.iloc[x][1] = Fundvalue
-            else :
-                ValuesMatrix.iloc[x][1] = ValuesMatrix.iloc[x-1][3]
-    
-            ValuesMatrix.iloc[x][2] = ValuesMatrix.iloc[x][1]*RealReturn
-            ValuesMatrix.iloc[x][3] = ValuesMatrix.iloc[x][2] + ValuesMatrix.iloc[x][1] 
-            ValuesMatrix.iloc[x][5] = ValuesMatrix.iloc[x][3]
-    
-        elif ((x + age) >=  RetireMentAge) :
-        
-            ValuesMatrix.iloc[x][0] = age + x
-            
-        
-            if (((12*TargetIncome/ValuesMatrix.iloc[x-1][3]) <= MaxDrawdown) & ((12*TargetIncome/ValuesMatrix.iloc[x-1][3]) >= MinDrawdown)) :
-                ValuesMatrix.iloc[x][1] = ValuesMatrix.iloc[x-1][3] - 12*TargetIncome
-                ValuesMatrix.iloc[x][2] = ValuesMatrix.iloc[x][1]*RealReturn
-                ValuesMatrix.iloc[x][3] = ValuesMatrix.iloc[x][2] + ValuesMatrix.iloc[x][1]
-                ValuesMatrix.iloc[x][4] = TargetIncome
-        
-            elif ((12*TargetIncome/ValuesMatrix.iloc[x-1][3]) > MaxDrawdown):
-                ValuesMatrix.iloc[x][1] = ValuesMatrix.iloc[x-1][3]*(1-MaxDrawdown)
-                ValuesMatrix.iloc[x][2] = ValuesMatrix.iloc[x][1]*RealReturn
-                ValuesMatrix.iloc[x][3] = ValuesMatrix.iloc[x][2] + ValuesMatrix.iloc[x][1]
-                ValuesMatrix.iloc[x][4] = ValuesMatrix.iloc[x-1][3]*MaxDrawdown/12
-        
-            elif ((12*TargetIncome/ValuesMatrix.iloc[x-1][3]) < MinDrawdown):
-                ValuesMatrix.iloc[x][1] = ValuesMatrix.iloc[x-1][3]*(1-MinDrawdown)
-                ValuesMatrix.iloc[x][2] = ValuesMatrix.iloc[x][1]*RealReturn
-                ValuesMatrix.iloc[x][3] = ValuesMatrix.iloc[x][2] + ValuesMatrix.iloc[x][1]
-                ValuesMatrix.iloc[x][4] = ValuesMatrix.iloc[x-1][3]*MinDrawdown/12
-            
-            ValuesMatrix.iloc[x][5] = ValuesMatrix.iloc[x][3]
-            
-    ValuesMatrix = ValuesMatrix.rename(columns = {0: 'Age', 1: 'Start fund value' , 2: 'Growth', 3: 'Retirement fund value', 4: 'Retirement income', 5: 'Real retirement fund value'}, inplace = False)
-    
-    RetirementGraph = px.bar(ValuesMatrix, x= 'Age', y='Real retirement fund value')
-
-    
-    RetirementIncome = px.bar(ValuesMatrix[ValuesMatrix['Age'] >= Client_Retirement_Age], x= 'Age', y= 'Retirement income')
-
-    
-    #Formatting outputs
-    Client_GrowthRate = round(Client_GrowthRate*100,1)
-    Client_Inflation = round(Client_Inflation*100,1)
-    
-    
-    return 'You are {} years of age'.format(Client_age), 'You want to retire at {} years of age'.format(Client_Retirement_Age), 'You will live {} years in retirement'.format(TermInRetirement) , slidervalues,'You want to invest R{}'.format(InvestmentAmount) , '{}% nominal growth per year'.format(Client_GrowthRate),  '{}% inflation per year'.format(Client_Inflation) , 'You require monthly income of R{} in retirement'.format(Client_target_Income), RetirementGraph, RetirementIncome
 
